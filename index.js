ff8213c63f023c81f2aade8038bb8e8a6dc6c84a
niththish <niththish21@gmail.com>
2022-11-09 19:24:11 +0530

    initial server setup with user route

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const express = require("express");
 
+const userRouter = require("./routes/user");
+
 const app = express();
+app.use("/api/user", userRouter);
+
+app.listen(5000, () => {
+  console.log("server started...");
+});
