849eb00bcf9b256083117e4a7f0dd159f6256f9a
niththish <niththish21@gmail.com>
2022-11-09 20:56:18 +0530

    addded a bcrypt encryption for user model

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 const addressSchema = require("./address");
 const cartSchema = require("./cart");
 
@@ -22,6 +23,12 @@ const userSchema = mongoose.Schema({
   },
   address: addressSchema,
   cart: cartSchema,
+});
+
+userSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
 module.exports = mongoose.model("user", userSchema);
