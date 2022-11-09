89b46c1609aa806d0d992fd9ac6b80cf67d44d8d
niththish <niththish21@gmail.com>
2022-11-09 18:46:09 +0530

    created users schema

---
+++ models/user.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, "username required"],
+    minLength: [4, "username must be atleast 4 characters"],
+  },
+  password: {
+    type: String,
+    required: [true, "password required"],
+  },
+  email: {
+    type: String,
+    required: [true, "email required"],
+  },
+  mobile: {
+    type: String,
+    required: [true, "mobile number required"],
+  },
+});
