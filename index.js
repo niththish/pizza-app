3009eb531c2cf7237a9956e1ead650546ac67995
niththish <niththish21@gmail.com>
2022-11-09 19:30:42 +0530

    added a middleware support for json data

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require("express");
 const userRouter = require("./routes/user");
 
 const app = express();
+
+app.use(express.json());
+
 app.use("/api/user", userRouter);
 
 app.listen(5000, () => {
