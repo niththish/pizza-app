66770ce7a6bd4dffd386edc32bb69c2c35bb260d
niththish <niththish21@gmail.com>
2022-11-12 16:47:40 +0530

    bug fix 'previous commit is not for orders' its for user cart

--- controllers/user_cart.js
+++ controllers/user_cart.js
@@ -1,6 +1,6 @@
 const userSchema = require("../models/user");
 
-const userOrders = async (req, res, next) => {
+const userCart = async (req, res, next) => {
   const _id = req.id;
   const orders = await userSchema.findOne({ _id }, { cart: 1 });
   if (orders) {
@@ -10,4 +10,4 @@ const userOrders = async (req, res, next) => {
   }
 };
 
-module.exports = userOrders;
+module.exports = { userCart };
--- routes/user.js
+++ routes/user.js
@@ -3,12 +3,12 @@ const signupController = require("../controllers/signup");
 const loginController = require("../controllers/login");
 const updateUserController = require("../controllers/update_user");
 const authVerification = require("../middleware/verifyToken");
-const userOrdersController = require("../controllers/user_orders");
+const { userCart } = require("../controllers/user_cart");
 
 const router = express.Router();
 router.post("/signup", signupController);
 router.post("/login", loginController);
 router.patch("/", authVerification, updateUserController);
-router.get("/orders", authVerification, userOrdersController);
+router.get("/cart", authVerification, userCart);
 
 module.exports = router;
