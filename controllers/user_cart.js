133b7161e4021a0ff9e0d8823faa2cc58d4462a5
niththish <niththish21@gmail.com>
2022-11-13 14:20:57 +0530

    completed add to cart functionality

--- controllers/user_cart.js
+++ controllers/user_cart.js
@@ -10,4 +10,21 @@ const getCart = async (req, res, next) => {
   }
 };
 
-module.exports = { getCart };
+const addToCart = async (req, res, next) => {
+  const _id = req.id;
+  const { productId, quantity, price } = req.body;
+
+  if (!productId || !quantity || !price) {
+    return next("invalid cart item");
+  }
+
+  await userSchema.updateOne(
+    { _id },
+    { $push: { cart: { productId, quantity, price } } },
+    { new: true, runValidators: true }
+  );
+
+  res.json({ status: "added to cart successfully" });
+};
+
+module.exports = { getCart, addToCart };
--- routes/cart.js
+++ routes/cart.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const authVerification = require("../middleware/verifyToken");
-const { getCart } = require("../controllers/user_cart");
+const { getCart, addToCart } = require("../controllers/user_cart");
 
 const router = express.Router();
 router.get("/", authVerification, getCart);
+router.post("/", authVerification, addToCart);
 
 module.exports = router;
