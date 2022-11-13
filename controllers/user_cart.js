dca23ffdd750ed073d0dde4525e3eab89313a349
niththish <niththish21@gmail.com>
2022-11-13 14:56:51 +0530

    completed delete cart functionality

--- controllers/user_cart.js
+++ controllers/user_cart.js
@@ -27,4 +27,31 @@ const addToCart = async (req, res, next) => {
   res.json({ status: "added to cart successfully" });
 };
 
-module.exports = { getCart, addToCart };
+const deleteFromCart = async (req, res, next) => {
+  const _id = req.id;
+  const { productId, quantity, price } = req.body;
+
+  if (!productId || !quantity || !price) {
+    return next("invalid cart item");
+  }
+
+  await userSchema.updateOne(
+    { _id },
+    { $pull: { cart: { productId, quantity, price } } }
+  );
+
+  res.json({ status: "removed item from cart successfully" });
+};
+
+const deleteCart = async (req, res, next) => {
+  const _id = req.id;
+  await userSchema.updateOne(
+    { _id },
+    { $set: { cart: [] } },
+    { new: true, runValidators: true }
+  );
+
+  res.json({ status: "user cart deleted successfully" });
+};
+
+module.exports = { getCart, addToCart, deleteFromCart, deleteCart };
--- routes/cart.js
+++ routes/cart.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const authVerification = require("../middleware/verifyToken");
-const { getCart, addToCart } = require("../controllers/user_cart");
+const {
+  getCart,
+  addToCart,
+  deleteFromCart,
+  deleteCart,
+} = require("../controllers/user_cart");
 
 const router = express.Router();
 router.get("/", authVerification, getCart);
 router.post("/", authVerification, addToCart);
+router.delete("/item", authVerification, deleteFromCart);
+router.delete("/", authVerification, deleteCart);
 
 module.exports = router;
