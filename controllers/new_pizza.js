e26b1030406ad245467b8b9890ca875a7fda4cde
niththish <niththish21@gmail.com>
2022-11-14 16:58:42 +0530

    modified image url field

--- controllers/new_pizza.js
+++ controllers/new_pizza.js
@@ -4,10 +4,10 @@ const addPizza = async (req, res, next) => {
   if (!req.file) return next("image file required");
 
   const { name, price, type } = req.body;
-  const URL = process.env.BASE_URL;
-  const image = `${URL}/images/${req.file.filename}`;
-
   if (!name || !price || !type) return next("all fields are required");
+
+  const URL = process.env.BASE_URL;
+  const image = `${URL}/images/${type}/${req.file.filename}`;
 
   const pizza = pizzaSchema.create({ name, price, image, type });
   if (pizza) res.json({ status: "added new pizza item successfully" });
