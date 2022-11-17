49f773956866bd59a33d2b37b7a2192a5a5e470b
niththish <niththish21@gmail.com>
2022-11-17 15:25:42 +0530

    modified admin's component names to prevent name conflicts

--- frontend/src/app/admin/pizzas/pizzas.component.ts
+++ frontend/src/app/admin/pizzas/pizzas.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-pizzas',
   templateUrl: './pizzas.component.html',
-  styleUrls: ['./pizzas.component.scss']
+  styleUrls: ['./pizzas.component.scss'],
 })
-export class PizzasComponent implements OnInit {
+export class AdminPizzasComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
